3714f53f2d3c2b80ae84c1ae33f69b0595383efa
calavell <calavell@tcd.ie>
2020-12-02 17:08:30 +0000

    added journey tests

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,4 +2,30 @@ require 'journey'
 
 describe Journey do
   let(:journey) {described_class.new}
-end
+  #let(:station) { Station.new("Euston", 1) }
+  describe '#start_journey' do
+    it 'start_journey should return start station' do
+      journey.start_journey('Euston')
+      expect(journey.start_station).to eq 'Euston'
+    end
+  end
+
+  describe '#end_journey' do
+    it 'end_station should return the exit station' do
+      journey.end_journey('Euston')
+      expect(journey.end_station).to eq 'Euston'
+    end
+  end
+
+  describe '#in_journey?' do
+    it 'returns false when not in journey' do
+      expect(journey.in_journey?).to eq false
+    end
+
+    it 'returns true when in journey' do
+      journey.start_journey('Euston')
+      expect(journey.in_journey?).to eq true
+    end
+  end
+
+ end
